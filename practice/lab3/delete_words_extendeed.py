2b5d709f64d2affbd99d062d9c19ff567368cbb5
aleksanderpanasiuk <olekp249@gmail.com>
2022-11-07 13:39:35 +0100

    Optimized delete_words_extended function

--- practice/lab3/delete_words_extendeed.py
+++ practice/lab3/delete_words_extendeed.py
@@ -2,16 +2,20 @@ def delete_words_extended(sentence, letters):
     fitting_words = []
 
     for word in sentence.split():
-        fits = True
-
-        for letter in letters:
-            if word.count(letter[0]) >= letter[1]:
-                fits = False
+        fits = letters_in_word(word, letters)
 
         if fits:
             fitting_words.append(word)
 
     return " ".join(fitting_words)
+
+
+def letters_in_word(word, letters):
+    for letter in letters:
+        if word.count(letter[0]) >= letter[1]:
+            return False
+
+    return True
 
 
 my_sentence = "I literally can't deal with this drama right now."
