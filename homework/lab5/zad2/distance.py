70dcfd4995543028e8bece1ad5f1f206b295d3ad
aleksanderpanasiuk <olekp249@gmail.com>
2022-11-18 18:48:49 +0100

    Fixed wrong assumption about board size

--- homework/lab5/zad2/distance.py
+++ homework/lab5/zad2/distance.py
@@ -1,2 +1,9 @@
-def distance():
-    pass
+def distance(n, A, B):
+    x_distance = flat_distance(A[0], B[0], n)
+    y_distance = flat_distance(A[1], B[1], n)
+
+    return x_distance + y_distance
+
+
+def flat_distance(a, b, n):
+    return min(abs(a - b), (2*n) - abs(a-b) + 1)
