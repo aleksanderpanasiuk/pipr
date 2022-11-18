3f2247003a8afbf0287dff8b3475f76a0e613f9a
aleksanderpanasiuk <olekp249@gmail.com>
2022-11-18 17:47:30 +0100

    Implemented biggest sum function

---
+++ homework/lab5/zad1/biggest_sum.py
@@ -0,0 +1,14 @@
+def biggest_sum(substring_length, list):
+    if substring_length > len(list):
+        substring_length = len(list)
+
+    current_sum = sum(list[:substring_length])
+    max_sum = current_sum
+
+    for i in range(1, len(list)-substring_length):
+        current_sum += list[i+substring_length]
+        current_sum -= list[i]
+
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
