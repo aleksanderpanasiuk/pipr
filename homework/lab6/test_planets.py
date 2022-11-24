b88b13d5c63f616d5b291f9d55ee96d9c2385c4a
aleksanderpanasiuk <olekp249@gmail.com>
2022-11-24 21:37:15 +0100

    Written test for basic functions in Planet class

---
+++ homework/lab6/test_planets.py
@@ -0,0 +1,43 @@
+from planets import Planet
+import pytest
+
+
+def test_planet_name():
+    planet = Planet(3, 0, 0,  no_moons=1)
+    assert planet.name() == ""
+
+    planet.set_name("Ziemia")
+    assert planet.name() == "Ziemia"
+
+
+def test_planet_name_not_stringable():
+    with pytest.raises(ValueError):
+        planet = Planet(3, 0, 0, name=(1, 4, 2))
+        planet.name()
+
+
+def test_planet_position():
+    planet = Planet(3, 0, 0,  "Ziemia", no_moons=1)
+    assert planet.position() == (3, 0, 0)
+
+    planet.set_position((2, 1, 3))
+    assert planet.position() == (2, 1, 3)
+
+
+def test_planet_position_nan():
+    with pytest.raises(ValueError):
+        planet = Planet(3, 's', 0,  "Ziemia", no_moons=1)
+        planet.position()
+
+
+def test_planet_no_moons():
+    planet = Planet(3, 1, 0,  "Ziemia")
+    assert planet.number_of_moons() == 0
+
+    planet.set_number_of_moons(4)
+    assert planet.number_of_moons() == 4
+
+
+def test_planet_no_moons_innit():
+    planet = Planet(3, 1, 0,  "Ziemia", 31)
+    assert planet.number_of_moons() == 31
