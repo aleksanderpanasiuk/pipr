da5b222fa6040ceb909fb71c0190e1257cfd4d2f
aleksanderpanasiuk <olekp249@gmail.com>
2022-11-24 19:21:31 +0100

    Updated gitignore

---
+++ practice/lab6/definitons.py
@@ -0,0 +1,7 @@
+class Object:
+    pass
+
+
+class Person:
+    def introduce(self):
+        return f"My name is {self.first_name} and my last name is {self.last_name}"
---
+++ practice/lab6/main.py
@@ -0,0 +1,11 @@
+from definitons import Person
+
+jurek = Person()
+jurek.first_name = "Jurek"
+jurek.last_name = "Ogorek"
+
+karolina = Person()
+karolina.first_name = "Karolina"
+karolina.last_name = "Malina"
+
+print(jurek.introduce())
